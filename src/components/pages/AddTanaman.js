6376ac077bbaa7eef85d138e4b576630a4faeb3c
Busro Arshad <shandyabc313@gmail.com>
2022-12-04 08:26:51 +0700

    add tanaman

--- src/components/pages/AddTanaman.js
+++ src/components/pages/AddTanaman.js
@@ -1,13 +1,17 @@
 import React,{ useState } from 'react'
 import {Form, Button, Container} from 'react-bootstrap'
+import {useHistory} from 'react-router-dom'
 import axios from 'axios'
+import config from '../config/config';
 
 const AddTanaman = () => {
     const [nama,setNama] = useState("");
     const [kategori,setKategori] = useState("");
     const [lokasi,setLokasi] = useState("");
     const [deskripsi,setDeskripsi] = useState("");
-    const [image, setImage] = useState("");
+    const [img, setImg] = useState("");
+    const [msg, setMsg] = useState("");
+    const history = useHistory();
     // const [preview, setPreview] = useState("");
 
 //     const loadImage = (e) => {
@@ -15,13 +19,55 @@ const AddTanaman = () => {
 //     setImage(image);
 //     setPreview(URL.createObjectURL(image));
 //   };
-    
+const saveData= async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("nama", nama);
+    formData.append("kategori", kategori);
+    formData.append("lokasi", lokasi);
+    formData.append("deskripsi", deskripsi);
+    formData.append("img", img);
+    try{
+        // await axios.post(config.ROOT_URL + "/tanaman",{
+        //     nama:nama,
+        //     kategori:kategori,
+        //     lokasi:lokasi,
+        //     deskripsi:deskripsi,
+        //     img:img,
+
+        // });
+        await axios.post(config.ROOT_URL + "/tanaman", formData, {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      });
+         history("/homeAdmin");
+    } catch (error){
+        if (error.response){
+            setMsg(error.response.data.msg);
+        }
+    }
+    // const formData = new FormData();
+    // formData.append("nama", nama);
+    // formData.append("kategori", kategori);
+    // formData.append("lokasi", lokasi);
+    // formData.append("deskripsi", deskripsi);
+    // formData.append("img", img);
+    // try {
+    //   await axios.post(config.ROOT_URL + "/tanaman", formData);
+    //   history.push("/homeAdmin");
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
   return (
     <Container>
-        <Form>
+        <Form onSubmit={saveData}>
+        <p className="has-text-centered">{msg}</p>
       <Form.Group className="mb-3" >
         <Form.Label>Nama Tanaman</Form.Label>
-        <Form.Control type="text" placeholder="Nama Tanaman" value={nama} onChange={(e) => setNama(e.target.value)}/>
+        <Form.Control type="text" placeholder="Nama Tanaman" 
+        value={nama} onChange={(e) => setNama(e.target.value)}/>
       </Form.Group>
 
       <Form.Group className="mb-3">
@@ -41,7 +87,7 @@ const AddTanaman = () => {
 
       <Form.Group controlId="formFile" className="mb-3">
         <Form.Label>Gambar</Form.Label>
-        <Form.Control type="file" value={image} onChange={(e) => setImage(e.target.value)}/>
+        <Form.Control type="file" value={img} onChange={(e) => setImg(e.target.value)}/>
       </Form.Group>
       
       {/* {preview ? (
